eb6bcdfcf51e67778eea3eda74fd88019d6cdceb
andreasvidal <andrea.s.vidal00@gmail.com>
2022-06-13 16:32:11 -0400

    se modifica proyecto la estructura html y css, ahora funciona rating

--- main.js
+++ main.js
@@ -1,41 +1,32 @@
-// Get the modal
-const d = document
-const modal = d.getElementById("myModal");
-// Get the button that opens the modal
-const btn = d.getElementById("submit");
-// Get the <span> element that closes the modal
-const span = d.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-const selected = d.querySelector('#selected')
-btn.onclick = () => {
-        modal.style.display = "block";
-    }
-    // When the user clicks on <span> (x), close the modal
-span.onclick = () => {
-        modal.style.display = "none";
-    }
-    // When the user clicks anywhere outside of the modal, close it
-window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+let ratings = document.querySelectorAll('.circle'); //all divs with numbers//
+let btn = document.getElementById('btn'); // button //
+let evaluation = document.querySelector('.evaluation'); // evalutation div //
+let thankyou = document.querySelector('.thankyou'); // thank you div //
+let span = document.getElementById('rating'); // empty span where the user selected rating will be displayed//
 
-const reate = d.querySelector('.container-txt')
-selected.forEach(e => {
-    selected[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        let rating = selected[i].innerHTML;
+// Looping through all the number divs//
+for (let i = 0; i < ratings.length; i++) {
+    // Listening for a click event on each number div //
+    ratings[i].addEventListener('click', () => {
 
-        for (let i = 0; i < selected.lengt; i++) {
-            selected[i].classList.contains('select')
-            selected[i].classList.remove('select');
+        // getting the clicked number value property from 1 to 5//
+        let rating = ratings[i].innerHTML;
+
+        // looping through the numbers to check for a previously selected rating and removing if such. //
+        // Necessary in order to have only one rating selected at a time //
+        for (let i = 0; i < ratings.length; i++) {
+            ratings[i].classList.contains('selected')
+            ratings[i].classList.remove('selected');
         }
-        // add the selected class which highlights the selected rating //
-        selected[i].classList.add('select');
-        btn.addEventListener('click', () => {
-            reate.style.display = 'none'
-        })
 
+        // add the selected class which highlights the selected rating //
+        ratings[i].classList.add('selected');
+
+        // listening for a click on the btn in order to hide the current div and display the Thank You div with the chosen rating in the span //
+        btn.addEventListener('click', () => {
+            evaluation.style.display = 'none';
+            thankyou.style.display = 'block';
+            span.innerHTML = rating;
+        })
     })
-});
+}
